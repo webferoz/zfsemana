9e300f90f8a64ff77017238738876efa65bb28a0
OpenShift guest <pedro@webferoz.com.br>
2013-10-04 10:33:37 -0400

    controller ultimo

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -1,11 +1,14 @@
 <?php
+ 
 namespace Album\Controller;
-
+ 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-use Doctrine\ORM\EntityManager;
-
-class AlbumController extends AbstractActionController
+use Doctrine\ORM\EntityManager; 
+use Album\Form\AlbumForm; // <- adicionar isso
+use Album\Entity\Album; // <- adicionar isso
+ 
+class AlbumController extends AbstractActionController 
 {
     /**
      * @var Doctrine\ORM\EntityManager
@@ -27,24 +30,39 @@ class AlbumController extends AbstractActionController
  
     public function indexAction()
     {
-        
-        $albums =  $this->getEntityManager()->getRepository('Album\Entity\Album')->findAll();
-        
-        var_dump($album);
-
         return new ViewModel(array(
             'albums' => $this->getEntityManager()->getRepository('Album\Entity\Album')->findAll()
         ));
     }
-
+ 
     public function addAction()
     {
+        $form = new AlbumForm();
+        $form->get('submit')->setAttribute('label', 'Add');
+ 
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $album = new Album();
+ 
+            $form->setInputFilter($album->getInputFilter());
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                $album->populate($form->getData());
+                $this->getEntityManager()->persist($album);
+                $this->getEntityManager()->flush();
+ 
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('album');
+            }
+        }
+ 
+        return array('form' => $form);
     }
-
+ 
     public function editAction()
     {
     }
-
+ 
     public function deleteAction()
     {
     }
