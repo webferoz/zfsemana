0161d4b145e61fb13ab4c468259d85a99298049e
OpenShift guest <pedro@webferoz.com.br>
2013-10-04 09:50:04 -0400

    criacao do modulo album

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,48 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Album\Controller\Album' => 'Album\Controller\AlbumController',
+        ),
+    ),
+
+    'router' => array(
+        'routes' => array(
+            'album' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/album[/][:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Album\Controller\Album',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'album' => __DIR__ . '/../view',
+        ),
+    ),
+    
+    'doctrine' => array(
+        'driver' => array(
+            'Album_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/Album/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                     'Album\Entity' =>  'Album_driver'
+                ),
+            ),
+        ),
+    ),                
+);
